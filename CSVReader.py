d0aad813aa6ea544d1f7f0aca61d05715b0c1c16
Afshin <afshinshah77@gmail.com>
2019-05-28 14:37:10 +0430

    Writing to array with index added
    now we have item index in text file too

--- CSVReader.py
+++ CSVReader.py
@@ -3,11 +3,14 @@ import csv
 csv.register_dialect('aa',
                      delimiter=';',
                      skipinitialspace=True)
-results=[]
+results = []
 with open('C:\\Users\\Vesal\\Documents\\Uni\\97-2 Semester\\Information '
           'Retrival\\Project\\BX-CSV-Dump\\BX-Books.csv') as csvFile:
     reader = csv.reader(csvFile, dialect='aa')
     for row in reader:
         results.append(row[0])
+results.remove('ISBN')
+myFile = open("output.txt", "w")
+for item in results:
+    myFile.write("%s,%d \n" % (item ,results.index(item)))
 
-print(results)
