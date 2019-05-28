c90ecdebb607aa6737ac3bd6997d3f78f7d5f6d0
Afshin <afshinshah77@gmail.com>
2019-05-28 19:11:15 +0430

    UniqueStr2Int works now

--- UniqueStr2Int.py
+++ UniqueStr2Int.py
@@ -8,14 +8,15 @@ with open('C:\\Users\\Vesal\\Documents\\Uni\\97-2 Semester\\Information '
           'Retrival\\Project\\BX-CSV-Dump\\BX-Book-Ratings.csv') as csvFile:
     reader = csv.reader(csvFile, dialect='aa')
     for row in reader:
-        results.append(row[0])
-# results.remove('ISBN')
+        results.append(row[1])
+results.remove('ISBN')
 
 print('listing is done \n')
 
 d = dict([(y, x + 1) for x, y in enumerate(sorted(set(results)))])
 
 print('mapping is done')
+# print(d)
 myFile = open("output.txt", "w")
-for x in d:
+for x in results:
     myFile.write("%d \n" % d[x])
